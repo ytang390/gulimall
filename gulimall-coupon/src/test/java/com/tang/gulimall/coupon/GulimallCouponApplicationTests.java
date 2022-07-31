eb544bc60db1783be835a91dac934ccc8b85aeaf
tangyi <ytang390@gmail.com>
2022-07-31 14:22:09 +0800

    项目初始构建

---
+++ gulimall-coupon/src/test/java/com/tang/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tang.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
